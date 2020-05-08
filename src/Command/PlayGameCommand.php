7a15b0db91646568144b9015f11312f5ad482153
Anna Borzenko <aborzenko@dealersocket.com>
2020-05-08 23:01:51 +0200

    initial commit

---
+++ src/Command/PlayGameCommand.php
@@ -0,0 +1,26 @@
+<?php
+namespace Azul\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PlayGameCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'play';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Starts a new game.')
+            ->setHelp('This command allows you to create a user...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Whoa!');
+
+        return 0;
+    }
+}
