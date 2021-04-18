72ec692122cbab7219d2e3ee2f1b9eb0cb0a59f3
Anna Borzenko <aborzenko@dealersocket.com>
2021-04-18 22:29:38 +0200

    refactor FastestGameStrategy, move duplicated search to method

--- src/Player/Strategy/FastestGameStrategy.php
+++ src/Player/Strategy/FastestGameStrategy.php
@@ -19,44 +19,44 @@ class FastestGameStrategy extends AbstractStrategy
 					if ($this->board->isWallColorFilled($color, $row->getRowNumber())) {
 						continue;
 					}
-					# look for this color by factories, take it from anywhere
-					foreach ($factories as $factory) {
-						if ($factory->getTilesCount($color) === 0) {
-							continue;
-						}
-						return new Move($color, $row->getRowNumber(), $factory);
-					}
-					if ($table->getTilesCount($color)) {
-						return new Move($color, $row->getRowNumber(), $table);
+					# look for this color, take it from anywhere
+					$move = $this->getMoveByColorAndRow(
+						$factories,
+						$table,
+						$color,
+						$row->getRowNumber()
+					);
+					if ($move) {
+						return $move;
 					}
 				}
 			} else {
 				if ($row->getEmptySlotsCount() > 0) {
 					# row already got tiles, look for the same color anywhere
 					$color = $row->getMainColor();
-					foreach ($factories as $factory) {
-						if ($factory->getTilesCount($color) === 0) {
-							continue;
-						}
-						return new Move($color, $row->getRowNumber(), $factory);
-					}
-					# no needed tiles on factories, check table for this color
-					if ($table->getTilesCount($color)) {
-						return new Move($color, $row->getRowNumber(), $table);
+					$move = $this->getMoveByColorAndRow(
+						$factories,
+						$table,
+						$color,
+						$row->getRowNumber()
+					);
+					if ($move) {
+						return $move;
 					}
 				}
 			}
 		}
 
-		foreach ($factories as $factory) {
-			foreach ($this->getColors() as $color) {
-				if ($factory->getTilesCount($color) > 0) {
-					return new Move($color, Board::ROW_1, $factory);
-				} else {
-					if ($table->getTilesCount($color) > 0) {
-						return new Move($color, Board::ROW_1, $table);
-					}
-				}
+		# get any color to first row/on floor
+		foreach ($this->getColors() as $color) {
+			$move = $this->getMoveByColorAndRow(
+				$factories,
+				$table,
+				$color,
+				Board::ROW_1
+			);
+			if ($move) {
+				return $move;
 			}
 		}
 		return null;
@@ -67,5 +67,23 @@ class FastestGameStrategy extends AbstractStrategy
 		$colors = Color::getAll();
 		shuffle($colors);
 		return $colors;
+	}
+
+	private function getMoveByColorAndRow(
+		FactoryCollection $factories,
+		Table $table,
+		$color,
+		int $getRowNumber
+	): ?Move {
+		foreach ($factories as $factory) {
+			if ($factory->getTilesCount($color) === 0) {
+				continue;
+			}
+			return new Move($color, $getRowNumber, $factory);
+		}
+		if ($table->getTilesCount($color)) {
+			return new Move($color, $getRowNumber, $table);
+		}
+		return null;
 	}
 }
